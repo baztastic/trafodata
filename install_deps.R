e9d03553fe13a1d1db16d3b6e56481b0e83721f1
baztastic <pkunzip@gmail.com>
2017-09-20 15:05:01 +0100

    new dependencies procedure

---
+++ install_deps.R
@@ -0,0 +1,8 @@
+list.of.packages <- c("shiny", "ggplot2", "lubridate", "ggthemes", "devtools", "ggTimeSeries", "DT", "RPostgreSQL")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages)) install.packages(new.packages[!new.packages=="ggTimeSeries"])
+if('ggTimeSeries' %in% new.packages) devtools::install_github('Ather-Energy/ggTimeSeries')
+
+print("Done! Feel free to use shiny::runApp('.') now.")
